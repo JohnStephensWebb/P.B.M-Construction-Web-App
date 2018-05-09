154dcdb68f5de5151939afd903d64f093acbf907
John Stephens-Webb <JohnStephensWebb@Johns-MacBook-Pro-2.local>
2018-05-09 14:54:15 -0600

    Home page dynamically populates the three most recently added json objects as cards

--- javascript/homePage.js
+++ javascript/homePage.js
@@ -1,9 +1,46 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     console.log("DOM fully loaded and parsed");
 
+    // $.getJSON("items.json")
+    //   .done(function( json ) {
+    //     console.log( "JSON Data:," json);
+    //   })
+    //   .fail(function( jqxhr, textStatus, error ) {
+    //     var err = textStatus + ", " + error;
+    //     console.log( "Request Failed: " + err );
+    // });
+
+    $.getJSON("items.json", function(content){
+      console.log(content)
+
+
+      for (var i = content.length-3; i < content.length; i++) {
+      let cardHtml =
+        `<div class="col s12 m4">
+          <div class="card">
+            <div class="card-image">
+              <img src="${content[i].img}">
+              <span class="card-title">${content[i].title}</span>
+            </div>
+            <div class="card-content">
+              <p>${content[i].cardContent}</p>
+            </div>
+            <div class="card-action">
+              <a href="#">This is a link</a>
+            </div>
+          </div>
+        </div>`
+
+
+        let homeCards = $("#cards")
+        homeCards.append(cardHtml)
+      }
+    })
+
     $(".button-collapse").sideNav();
 
     $('.slider').slider();
+
    $('body').removeClass('fade-out');
 
 });
